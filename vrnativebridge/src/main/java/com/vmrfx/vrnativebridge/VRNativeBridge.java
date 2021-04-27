e2309ce98d082fd01a4cd3736f474156f67d15a2
Vladimir Voinea <voineavladimir@gmail.com>
2021-04-27 20:41:31 +0300

    add touch events

--- vrnativebridge/src/main/java/com/vmrfx/vrnativebridge/VRNativeBridge.java
+++ vrnativebridge/src/main/java/com/vmrfx/vrnativebridge/VRNativeBridge.java
@@ -1,7 +1,24 @@
 package com.vmrfx.vrnativebridge;
 
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+
+
 public class VRNativeBridge {
+    static
+    {
+        System.loadLibrary("vr-android-bridged");
+    }
+
     public static native void initialize();
     public static native void on_resize(int width, int height);
     public static native void render_frame();
+
+    public static native void set_activity(Activity activity);
+    public static native void set_asset_manager(AssetManager manager);
+
+    public static native void on_touch_event(MotionEvent event);
+    public static native void on_key_event(KeyEvent event);
 }
